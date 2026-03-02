364a87b3a004e16d45cbb56beadfb34f62e1c281
FarhadDipta <160616287+FarhadDipta@users.noreply.github.com>
2026-03-02 19:54:45 +0700

    1

---
+++ resources/views/about.blade.php
@@ -0,0 +1,18 @@
+<x-app-layout>
+    <div class="py-12">
+        <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white shadow-lg rounded-xl p-8">
+                <h1 class="text-2xl font-bold mb-6 text-gray-800">
+                    Biodata Mahasiswa
+                </h1>
+
+                <ul class="space-y-3 text-gray-700">
+                    <li><strong>Nama:</strong> Farhad Dipta Utama</li>
+                    <li><strong>NIM:</strong> 20230140206</li>
+                    <li><strong>Program Studi:</strong> Teknologi Informasi</li>
+                    <li><strong>Hobi:</strong> Makan</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
